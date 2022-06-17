8ed5a4a64a1f63b1176da3ebbe3b4bf2bd7a9128
Fabricio Suhet <fabricio.suhet@sempreceub.com>
2022-06-17 19:59:58 -0300

    Atualizações no código

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,11 +1,14 @@
 import sys
+from settings import Settings
 import pygame
 
 
 def run_game():
-    # Inicializa o jogo e cria um ojbeto para a tela
+    # Inicializa o pygame, as configurações e o objeto screen
     pygame.init()
-    screen = pygame.display.set_mode((1000, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     # Inicia o laço principal do jogo
     running = True
@@ -15,6 +18,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == quit:
                 sys.exit()
+        # Redesenha a tela a cada passagem do laço
+        screen.fill(ai_settings.bg_color)
+        # Deixa a tela mais recente visivel
         pygame.display.flip()
 
 
---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """Uma classe para armazenaar todas configurações
+    da Invasão Alienigena."""
+
+    def __init__(self):
+        """Inicializa as configurações do jogo"""
+        # Configurações da tela
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
